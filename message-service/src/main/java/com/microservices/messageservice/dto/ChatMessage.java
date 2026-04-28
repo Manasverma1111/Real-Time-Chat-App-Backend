675df54e281c499fb1ab03286dd0c988beab2a18
Manasverma1111 <vmanas79@gmail.com>
2026-04-28 23:28:20 +0530

    updates configs

--- message-service/src/main/java/com/microservices/messageservice/dto/ChatMessage.java
+++ message-service/src/main/java/com/microservices/messageservice/dto/ChatMessage.java
@@ -12,14 +12,3 @@ public class ChatMessage {
 	// NEW FIELD
 	private String senderName;
 }
-
-//package com.microservices.messageservice.dto;
-//
-//import lombok.Data;
-//
-//@Data
-//public class ChatMessage {
-//	private String content;
-//	private String senderId;
-//	private String roomId;
-//}
