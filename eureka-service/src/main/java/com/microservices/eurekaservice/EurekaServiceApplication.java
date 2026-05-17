bf41a77e0b8cad5c5e06bd17936550d2bc004c01
Manasverma1111 <vmanas79@gmail.com>
2026-05-17 17:46:13 +0530

    Improve configuration documentation

--- eureka-service/src/main/java/com/microservices/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/microservices/eurekaservice/EurekaServiceApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServiceApplication {
 
+//	The EurekaServiceApplication class is the entry point of the Spring Boot application
+//	and is annotated with @EnableEurekaServer to enable Eureka Server functionality.
+//	This allows the application to act as a service registry
+//	where other microservices can register themselves and discover other services.
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
